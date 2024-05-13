e7b23301c7816279ad3e44eef4f3bb200f2ccbd5
Striker1101 <goodluckodemilin@gmail.com>
2024-05-13 17:37:54 +0100

    changed chat on loan-swift

--- src/Chat.jsx
+++ src/Chat.jsx
@@ -6,7 +6,7 @@ const Chat = () => {
       const s1 = document.createElement("script");
       const s0 = document.getElementsByTagName("script")[0];
       s1.async = true;
-      s1.src = "https://embed.tawk.to/6637337107f59932ab3c2923/1ht3q1t0a";
+      s1.src = "https://embed.tawk.to/66423ff39a809f19fb306ed0/1htpcjk8u";
       s1.charset = "UTF-8";
       s1.setAttribute("crossorigin", "*");
       s0.parentNode.insertBefore(s1, s0);
