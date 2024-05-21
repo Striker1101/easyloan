de748b468d718faea1d47ba18f0770b79779e73c
striker1101 <goodluckodemilin@gmail.com>
2024-05-21 13:04:50 +0100

    changed chat loanswifter

--- src/Chat.jsx
+++ src/Chat.jsx
@@ -6,7 +6,7 @@ const Chat = () => {
       const s1 = document.createElement("script");
       const s0 = document.getElementsByTagName("script")[0];
       s1.async = true;
-      s1.src = "https://embed.tawk.to/66423ff39a809f19fb306ed0/1htpcjk8u";
+      s1.src = "https://embed.tawk.to/664c8a69981b6c564772e003/1hudfpbk6";
       s1.charset = "UTF-8";
       s1.setAttribute("crossorigin", "*");
       s0.parentNode.insertBefore(s1, s0);
