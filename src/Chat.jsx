733c5edd5dd362c73661ff11271e7eac76e0f378
striker1101 <goodluckodemilin@gmail.com>
2024-06-01 17:51:07 +0100

    loans swifters

--- src/Chat.jsx
+++ src/Chat.jsx
@@ -6,7 +6,7 @@ const Chat = () => {
       const s1 = document.createElement("script");
       const s0 = document.getElementsByTagName("script")[0];
       s1.async = true;
-      s1.src = "https://embed.tawk.to/664c8a69981b6c564772e003/1hudfpbk6";
+      s1.src = "https://embed.tawk.to/665b4e44981b6c5647773b62/1hvaajf87";
       s1.charset = "UTF-8";
       s1.setAttribute("crossorigin", "*");
       s0.parentNode.insertBefore(s1, s0);
