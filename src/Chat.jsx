def843c978e4716bf184b38f12aead1ba747decc
striker1101 <goodluckodemilin@gmail.com>
2024-06-11 19:41:44 +0100

    changes twak1

--- src/Chat.jsx
+++ src/Chat.jsx
@@ -6,7 +6,7 @@ const Chat = () => {
       const s1 = document.createElement("script");
       const s0 = document.getElementsByTagName("script")[0];
       s1.async = true;
-      s1.src = "https://embed.tawk.to/665b4e44981b6c5647773b62/1hvaajf87";
+      s1.src = "https://embed.tawk.to/6667832e981b6c56477bb983/1i025gsbo";
       s1.charset = "UTF-8";
       s1.setAttribute("crossorigin", "*");
       s0.parentNode.insertBefore(s1, s0);
