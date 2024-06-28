3b191c35f8fa7b44ffd2773235717545f7eee440
striker1101 <goodluckodemilin@gmail.com>
2024-06-28 17:58:21 +0100

    change tawk https://embed.tawk.to/667ed71eeaf3bd8d4d158b0c/1i1fngm3m

--- src/Chat.jsx
+++ src/Chat.jsx
@@ -6,7 +6,7 @@ const Chat = () => {
       const s1 = document.createElement("script");
       const s0 = document.getElementsByTagName("script")[0];
       s1.async = true;
-      s1.src = "https://embed.tawk.to/666899499a809f19fb3c8488/1i049di1m";
+      s1.src = "https://embed.tawk.to/667ed71eeaf3bd8d4d158b0c/1i1fngm3m";
       s1.charset = "UTF-8";
       s1.setAttribute("crossorigin", "*");
       s0.parentNode.insertBefore(s1, s0);
