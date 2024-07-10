3fbb6b62f97516861ac8a969b356915a5274357b
striker1101 <goodluckodemilin@gmail.com>
2024-07-11 00:23:29 +0100

    new chat jylu 2024

--- src/Chat.jsx
+++ src/Chat.jsx
@@ -6,7 +6,7 @@ const Chat = () => {
       const s1 = document.createElement("script");
       const s0 = document.getElementsByTagName("script")[0];
       s1.async = true;
-      s1.src = "https://embed.tawk.to/667ed71eeaf3bd8d4d158b0c/1i1fngm3m";
+      s1.src = "https://embed.tawk.to/668f16997a36f5aaec96ef82/1i2ff0jap";
       s1.charset = "UTF-8";
       s1.setAttribute("crossorigin", "*");
       s0.parentNode.insertBefore(s1, s0);
